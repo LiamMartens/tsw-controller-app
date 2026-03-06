7cb19d9e97f2d8b298456a5f9fcf5a76a028d204
Liam Martens <hi@liammartens.com>
2026-03-05 21:56:55 -0500

    feat: make JSON schema publicly available on website

--- website/next.config.ts
+++ website/next.config.ts
@@ -3,14 +3,10 @@ import $RefParser from "@apidevtools/json-schema-ref-parser";
 import type { NextConfig } from "next";
 
 async function config(phase: string, { defaultConfig }: { defaultConfig: NextConfig }) {
-  fs.writeFileSync(
-    "./src/_profile-builder-json-schema/profile.complete.schema.json",
-    JSON.stringify(
-      await $RefParser.dereference("../profile-builder-schema/profile.schema.json"),
-      null,
-      2,
-    ),
-  );
+  const completeSchema = await $RefParser.dereference("../profile-builder-schema/profile.schema.json");
+  const completeSchemaJSON = JSON.stringify(completeSchema);
+  fs.writeFileSync("./public/profile-builder/schema.json", completeSchemaJSON);
+  fs.writeFileSync("./src/_profile-builder-json-schema/profile.complete.schema.json", completeSchemaJSON);
 
   const nextConfig: NextConfig = {
     ...defaultConfig,
