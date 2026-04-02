edae657125804e538b142dccedcd1b075e64574a
Liah Martens <hi@liahmartens.com>
2026-04-01 21:43:11 -0400

    fix: android statusbar

--- virtual-controller/src/app/main.tsx
+++ virtual-controller/src/app/main.tsx
@@ -2,9 +2,12 @@ import "./style.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { StatusBar } from "@capacitor/status-bar";
+
+StatusBar.setOverlaysWebView({ overlay: false });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
