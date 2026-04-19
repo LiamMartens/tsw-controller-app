ca719889629a59b66a42a580e2d141caf9ee0d90
Liah Martens <hi@liahmartens.com>
2026-04-19 11:27:34 -0400

    feat: Updated website dependencies

--- website/next.config.ts
+++ website/next.config.ts
@@ -1,19 +1,24 @@
-import fs from "node:fs"
+import fs from "node:fs";
 import $RefParser from "@apidevtools/json-schema-ref-parser";
 import type { NextConfig } from "next";
 
 async function config(phase: string, { defaultConfig }: { defaultConfig: NextConfig }) {
-  const completeSchema = await $RefParser.dereference("../profile-builder-schema/profile.schema.json");
+  const completeSchema = await $RefParser.dereference(
+    "../profile-builder-schema/profile.schema.json",
+  );
   const completeSchemaJSON = JSON.stringify(completeSchema);
   fs.writeFileSync("./public/profile-builder/schema.json", completeSchemaJSON);
-  fs.writeFileSync("./src/_profile-builder-json-schema/profile.complete.schema.json", completeSchemaJSON);
+  fs.writeFileSync(
+    "./src/_profile-builder-json-schema/profile.complete.schema.json",
+    completeSchemaJSON,
+  );
 
   const nextConfig: NextConfig = {
     ...defaultConfig,
-    output: 'export',
+    output: "export",
   };
 
-  return nextConfig
+  return nextConfig;
 }
 
-export default config
+export default config;
