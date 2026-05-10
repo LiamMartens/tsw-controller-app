b11e67de797c27fb6f7fd97e3c9984610c6796b2
Liah Martens <hi@liahmartens.com>
2026-05-10 17:57:56 -0400

    feat: add profile builder

---
+++ profile-builder/app/index.tsx
@@ -0,0 +1,10 @@
+import "./app.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+
+const app = document.getElementById("app")!;
+
+const root = ReactDOM.createRoot(app);
+
+root.render(React.createElement(App));
---
+++ profile-builder/bin/generate_ts.mjs
@@ -0,0 +1,36 @@
+import fs from "node:fs";
+import ts from "typescript";
+import schema from "../app/schema.json" with { type: "json" };
+import openapiTS, { astToString } from "openapi-typescript";
+
+/** @type {(node: ts.Node) => node is ts.InterfaceDeclaration} */
+const isInterfaceDeclaration = (node) =>
+  node.kind == ts.SyntaxKind.InterfaceDeclaration;
+
+/** @type {(node: ts.Node) => node is ts.PropertySignature} */
+const isPropertySignature = (node) =>
+  node.kind == ts.SyntaxKind.PropertySignature;
+
+const ast = await openapiTS({
+  openapi: "3.1",
+  components: {
+    schemas: {
+      schema,
+    },
+  },
+});
+
+/** @type {ts.InterfaceDeclaration} */
+const components = ast.find(
+  (node) => isInterfaceDeclaration(node) && node.name.text === "components",
+);
+
+/** @type {ts.PropertySignature} */
+const schemas = components.members.find(
+  (member) => isPropertySignature(member) && member.name.text === "schemas",
+);
+
+components.name = ts.factory.createIdentifier("profile_builder_schema");
+components.members = schemas.type.members[0].type.members;
+
+fs.writeFileSync("./app/profile_builder_schema.d.ts", astToString(components));
